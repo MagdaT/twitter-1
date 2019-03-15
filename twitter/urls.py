914d9f7eecc71ed536e8c01d2215963af9d3d090
mnowotka <mmmnow@gmail.com>
2019-03-15 23:02:20 +0100

    User login, logout, registration, password reset and profile implemented Main webpage shows all tweets.

--- twitter/urls.py
+++ twitter/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from django.views import generic
+from twitter import views
 
 app_name = 'twitter'
 urlpatterns = [
-    path('', generic.TemplateView.as_view(template_name='twitter/index.html'),
-         name='index'),
+    path('', views.TweetListView.as_view(), name='index'),
+    path('register/', views.RegisterView.as_view(), name='register'),
+    path('profile/', views.ProfileView.as_view(), name='profile'),
+    path('compose/', views.ComposeView.as_view(), name='compose'),
 ]
--- twitter/views.py
+++ twitter/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
+from django.views import View
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import CreateView
 
-# Create your views here.
+from twitter import models
+from twitter import forms
+
+
+class TweetListView(View):
+
+    def get(self, request):
+        tweets = models.Tweet.objects.all().order_by('-creation_date')
+        return render(request, 'twitter/index.html', {'tweets': tweets})
+
+
+class RegisterView(View):
+
+    def get(self, request):
+        return render(request, 'twitter/register.html',
+                      {'form': forms.UserRegisterForm()})
+
+
+class ProfileView(LoginRequiredMixin, View):
+
+    def get(self, request):
+        tweets = models.Tweet.objects.filter(
+            author=request.user).order_by('-creation_date')
+        return render(request, 'twitter/profile.html', {'tweets': tweets})
+
+
+class ComposeView(LoginRequiredMixin, CreateView):
+    model = models.Tweet
+    form_class = forms.TweetForm
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
