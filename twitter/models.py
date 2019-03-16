bdd5f5d5cd3fa53be9ab90b7f8bd72561206ced2
mnowotka <mmmnow@gmail.com>
2019-03-16 19:38:06 +0100

    Added Message model.

--- twitter/models.py
+++ twitter/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 
 TWITTER_MAXIMUM_TWEET_LENGTH = 280
 TWITTER_MAXIMUM_COMMENT_LENGTH = 60
+TWITTER_MAXIMUM_MSG_LENGTH = 64
 
 
 class Tweet(models.Model):
@@ -23,4 +24,20 @@ class Comment(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.text
+        return self.content
+
+
+class Message(models.Model):
+    title = models.CharField(max_length=TWITTER_MAXIMUM_MSG_LENGTH)
+    content = models.TextField()
+    date_send = models.DateTimeField(auto_now_add=True)
+    sender = models.ForeignKey(User, on_delete=models.CASCADE,
+                               related_name="user_from")
+    recipient = models.ForeignKey(User, on_delete=models.CASCADE,
+                                  related_name="user_to")
+    read = models.BooleanField(default=False)
+    blocked = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'from: {self.sender} to: ' \
+               f'{self.recipient} message: {self.content[0:20]}'
