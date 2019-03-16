f8d5c188f7db9fd52dd682712bc53d258608712a
mnowotka <mmmnow@gmail.com>
2019-03-16 19:01:11 +0100

    Comment model added.

--- twitter/models.py
+++ twitter/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 TWITTER_MAXIMUM_TWEET_LENGTH = 280
+TWITTER_MAXIMUM_COMMENT_LENGTH = 60
 
 
 class Tweet(models.Model):
@@ -13,3 +14,13 @@ class Tweet(models.Model):
         return '[{}] TWEET by {}: {}'.format(
             self.creation_date,
             self.author, self.content[:20])
+
+
+class Comment(models.Model):
+    content = models.CharField(max_length=TWITTER_MAXIMUM_COMMENT_LENGTH)
+    date_comment = models.DateTimeField(auto_now_add=True)
+    tweet = models.ForeignKey(Tweet, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.text
